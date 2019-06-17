0db8b2a6818e87c9c1711095299475542d7d909b
Ocpew36 <quanglmph05475@fpt.edu.vn>
2019-06-17 22:22:29 +0700

    abc

---
+++ Session1/index.js
@@ -0,0 +1,17 @@
+// day la doan code cong 2 so
+// const x = Number(prompt("name x"));
+// const y = Number(prompt("name y"));
+
+// console.log(x*y);
+
+// loop 
+
+// comment code
+
+// for(let y = 1;y<101;y++){
+    // console.log (y);
+    // console.log("");
+// }
+for(let y = 1;y<101;y+5){
+
+}
