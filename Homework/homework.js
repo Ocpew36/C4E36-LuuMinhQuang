d3f14209a4e3bbfc1beed7eb844becad2ee85ccd
Ocpew36 <51919684+Ocpew36@users.noreply.github.com>
2019-07-11 13:21:08 +0700

    Add files via upload

---
+++ Homework/homework.js
@@ -0,0 +1,68 @@
+// 1.Why should we use functions at all?
+// Để code gọn hơn và dễ chỉnh sử hơn khi cần
+
+// 2.How to define/declare a function?
+// Function là từ khóa bắt buộc và tên của hàm
+
+// 3.How to call/use a function?
+// tên hàm và giá trị muốn gọi
+
+// 4.What is return, why and how do we use it?
+// return là lệnh dùng để trả về giá trị mà người dùng muốn hàm trả về. vì nếu k có return chúng ta không thể thay đổi giá trị ban đầu
+
+// 5.Do we have to use return in every function?
+// chúng ta không cần phải sử dụng return trong mọi trường hợp
+
+// 6.What are function arguments/parameters, why and how we use it?
+// arguments và parameters là các tham số truyền vào của hàm, tham số truyền vào cần thiết để chúng ta truyền giá trị mà ta muốn vào hàm.
+
+
+// How to use function from a different file other than our currently working file?
+// Gọi tên function.chức năng muốn gọi
+
+
+// 1
+// const str = (string) => {
+//     const result = string;
+//     console.log(result) ;
+// }
+// str ("HelloWorld!")
+// str ("HelloWorld!")
+// str ("HelloWorld!")
+
+
+// 2.
+// const total = (a, b) => {
+//     console.log(a + b);
+// }
+// total (4, 6);
+
+// 3.
+// for (let i = 0; i  < 30; i ++){
+
+    
+
+// }
+
+
+// 6.
+// const remove_dollar_sign = ($8000) => {
+//     const result = 
+//     console.log( remove_dollar_sign.replace ('$',''));
+    
+// }
+
+
+// 7.
+
+// string_with_no_dollars = remove_dollar_sign("$80% percent of $life is to show $up")
+// if (string_with_no_dollars == "80% percent of life is to show up"){
+//    console.log("Your function is correct")}
+// else{
+//    console.log("Oops, there's a bug")}
+
+// 9.
+const get_even_list = (a, b, c, d, f) => {
+    return (b, f);
+}
+get_even_list([1, 4, 5, -1, 10])
